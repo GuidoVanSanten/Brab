d42a2b25cc4ce7fac70155be48e2608f69520358
Guido van Santen <thefidgitive@gmail.com>
2013-02-23 16:26:48 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BraB::Application.config.secret_token = '49d03d71a46c2bd593c1d6c002aafe0836b1f004b6415b264b427df3f191c36f8c6f20badd53b7e344b9ae0037942b4d08c41b7d8262233af0ffe2a149adfc43'
